17cd152f643159bba218997a50234933082b2463
ShaiD9 <fffdiamant.orange@gmail.com>
2024-12-04 12:16:29 +0100

    Initial commit

---
+++ src/main/java/backend/chaTop/apiChaTop/ApiChaTopApplication.java
@@ -0,0 +1,13 @@
+package backend.chaTop.apiChaTop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChaTopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiChaTopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/chaTop/apiChaTop/ApiChaTopApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.chaTop.apiChaTop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChaTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
